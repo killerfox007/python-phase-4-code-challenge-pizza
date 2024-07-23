eee4bcdcd3c1fbcb087ed79f5e22fe5fe6a88d32
Kaleb Fox <kalebfox7@yahoo.com>
2024-07-22 23:14:38 -0400

    done

--- server/app.py
+++ server/app.py
@@ -24,6 +24,71 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class RestrauntResource(Resource):
+    def get(self):
+        restraunt = [{
+            "id":rest.id,
+            "name":rest.name,
+            "address":rest.address
+            } for rest in Restaurant.query.all()]
+        if restraunt:
+            return restraunt, 200
+
+
+
+
+class RestrauntResourceid(Resource):
+    def get(self,id):
+        restraunt = Restaurant.query.get(id)
+        print(restraunt)
+        if restraunt:
+            return restraunt.to_dict(), 200
+        else:
+            return {"error": "Restaurant not found"}, 404
+    def delete(self,id):
+        restraunt = Restaurant.query.get(id)
+        if restraunt:
+            db.session.delete(restraunt)
+            db.session.commit()
+            return {}, 204
+        else:
+            return {"error": "Restaurant not found"}, 404
+
+
+class PizzaResource(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict() for pizza in Pizza.query.all()]
+        return pizzas
+
+class RestPizzaResource(Resource):
+    def post(self):
+        data = request.get_json()
+        price = data.get("price")
+        pizza_id = data.get("pizza_id")
+        restaurant_id = data.get("restaurant_id")
+        pizza_find = Pizza.query.get(pizza_id)
+        restaurant_find = Restaurant.query.get(restaurant_id)
+        if pizza_find:
+            if restaurant_find:
+                    if price >= 1 and price <= 30:
+                        restpizza = RestaurantPizza(price=price,pizza_id=pizza_id,restaurant_id=restaurant_id)
+                        db.session.add(restpizza)
+                        db.session.commit()
+                        return restpizza.to_dict(), 201
+                    else:
+                        return {"errors": ["validation errors"]}, 400
+
+            else:
+                return {"errors": ["validation errors"]}, 400
+        else:
+            return {"errors": ["validation errors"]}, 400
+        
+
+
+api.add_resource(PizzaResource, "/pizzas")
+api.add_resource(RestrauntResource, "/restaurants")
+api.add_resource(RestrauntResourceid, "/restaurants/<int:id>")
+api.add_resource(RestPizzaResource, "/restaurant_pizzas")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -22,7 +22,10 @@ class Restaurant(db.Model, SerializerMixin):
 
     # add relationship
 
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade='all, delete-orphan')
     # add serialization rules
+
+    serialize_rules =("-restaurant_pizzas.restaurant","-restaurant_pizzas",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,9 +39,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    pizzas = db.relationship("RestaurantPizza", back_populates="pizza", cascade='all, delete-orphan')
+    
     # add serialization rules
-
+    serialize_rules = ('-pizzas.pizza',)
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
@@ -48,12 +52,21 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
 
     # add relationships
-
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="pizzas")
     # add serialization rules
-
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.pizzas',)
     # add validation
-
+    @validates('price')
+    def validate_price(self, key, price):
+        if price >= 1 and price <= 30:
+            return price
+        else:
+            raise ValueError
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
